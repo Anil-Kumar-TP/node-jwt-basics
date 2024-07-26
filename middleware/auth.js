1ef0d3d3ba9c95de71b66fc45adfcf15312566e0
Anil <anil6996f@gmail.com>
2024-07-26 12:27:53 +0530

    Learned JWT basics

---
+++ middleware/auth.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+const { UnauthenticatedError } = require('../errors');
+
+const authorizationMiddleware = async (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith('Bearer')) {
+        throw new UnauthenticatedError('No token provided');
+    }
+    const token = authHeader.split(' ')[1] //got back the token
+ 
+     try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET) //verifying
+        const { id, username } = decoded;
+        req.user = { id, username };
+        next()
+    } catch (error) {
+        throw new UnauthenticatedError('Not authorized to access this route');
+    }
+}
+
+module.exports = authorizationMiddleware;
